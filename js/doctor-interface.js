80e840455acbbc157242ae687cf3714a06b1026d
Skye Atwood <atwood.skye@gmail.com>
2017-01-06 09:42:20 -0800

    create Doctor function, work on API reponse

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,4 +1,10 @@
 var apiKey = require('./../.env').apiKey;
+var doctors = [];
+
+Doctor = function(name) {
+  this.name = name;
+};
+
 
 
 $(document).ready(function() {
@@ -7,8 +13,13 @@ $(document).ready(function() {
     var medIssue = $('#issue').val();
     $('#issue').val("");
     $('.showDoctors').text("You searched for help with" + medIssue + ".")
-  $.get('https://api.betterdoctor.com/2016-03-01/specialties?query='+ medIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey, function(response) {
-      $('.showDoctors').append("<li>" + response.profile + "</li>")
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medIssue + '&location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&skip=0&limit=10&user_key=' + apiKey).then(function(response) {
+    console.log(response);
+    var doctors = response.doctors;
+    for (i = 0; i <= doctors.length; i++) {
+      var name = doctors[i].name;
+      $('.showDoctors').append("<li>" + response.name + "</li>")
+    }
     });
   });
 });
